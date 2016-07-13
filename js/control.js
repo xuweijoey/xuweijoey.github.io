ed5177419a3226690cac82565a828fcd88bfe842
xuweijoey <Joey@Weis-MacBook-Pro.local>
2016-07-13 14:48:45 -0400

    July13 updates

---
+++ js/control.js
@@ -0,0 +1,24 @@
+/*
+$(document).ready(function() {
+
+ $("#content").fadeOut();
+
+ setTimeout(function(){
+ 	$("#content").text("is dooooooooooooooooope");
+ }, 400);
+ 
+ $("#content").fadeIn();
+
+ $("#content").mouseenter(function(){
+ 	$(this).css("border", "solid 2px #990033");
+ });
+
+ $("#content").mouseleave(function(){
+ 	$(this).css("border", "none");
+ });
+
+
+//$("#content").append( "<p>Test</p>" );
+
+});
+*/
